21b72cbf15bb2cc3647de3a2b2f0be24c9790c4c
londono19 <slondono.2007@hotmail.com>
2022-11-25 17:32:31 -0800

    added log in function, plus styles

--- src/App.js
+++ src/App.js
@@ -3,15 +3,45 @@ import { Route, Switch } from "react-router-dom";
 import ToWatchPage from "./components/ToWatchPage";
 import Navbar from "./components/Navbar";
 import About from "./components/About";
+import logIn from "./components/logIn";
 import Error from "./components/Error";
 import { nanoid } from "nanoid";
+import jwt_decode from "jwt-decode";
 import MoviePage from "./components/MoviePage";
 
 function App(props) {
   const [tasks, setTasks] = useState([]);
   const [toWatch, setToWatch] = useState([]);
+  const [user, setuser] = useState({});
 
-
+  function handleCallBackResponse(response){
+    //console.log("encoded jwt id toekn" + response.credential);
+    let userObject = jwt_decode(response.credential);
+    setuser(userObject);
+    console.log(userObject);
+    document.getElementById("signInDiv").style.display = "none";
+    document.getElementById("outButton").style.display = "flex";
+    document.getElementById("outButton").style.justifyContent = "right";
+  }
+  //global google
+  useEffect(()=>{
+    /*global google*/
+    google.accounts.id.initialize({
+      client_id: "227959365284-n2oldup2n4a9sjscmnskfcmfir3bu151.apps.googleusercontent.com",
+      callback: handleCallBackResponse,
+    });
+    
+    google.accounts.id.renderButton(
+      document.getElementById("signInDiv"),
+      {
+        theme: "outline",
+        size: "large",
+        text: "signin_with"
+      }
+      )
+  }, [])
+  
+  
   function addTask(name, moviePoster) {
     const newTask = { id: `todo-${nanoid()}`, name, completed: false, moviePoster };
     let newArrTask = [];
@@ -22,21 +52,19 @@ function App(props) {
     //passes the new task to setTask using a split function appending it to the end of the task list
     setTasks(newArrTask);
   }
+  
+  function handleSignOut(event){
+    setuser({});
+    document.getElementById("signInDiv").style.display = "flex";
+  }
 
-  /*let myTimer = setInterval(printTaks, 3000);
-
-  function printTaks(){
-    for(let i = 0; i < tasks.length; i++){
-      console.log(tasks);
-    }
-  }*/
   //tasks on parent so that we can add a movie from 
   //const [tasks, setTasks] = useState([]);
   //*******  RETURN STARTS HERE  **********
   return (
     <>
     
-      <Navbar />
+      <Navbar user={user} signOut={handleSignOut}/>
       <Switch>
         <Route path="/" exact>
           <ToWatchPage tasks={tasks} setTasks={setTasks} addTask={addTask}/>
@@ -44,7 +72,6 @@ function App(props) {
         <Route path="/movie">
           <MoviePage tasks={tasks} setTasks={setTasks} addTask={addTask}/>
         </Route>
-
         <Route path="/about" component={About} />
         <Route component={Error} />
       </Switch>
---
+++ src/components/logIn.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function logIn(props) {
+  return (
+    <>
+    </>
+  );
+}
+
+export default logIn;
